34f5936a33ae51a003e0148c0ae4b6e95b36e8f7
Marcin Fortuna <marcin.fortuna@onet.eu>
2025-07-14 10:27:06 +0200

    Add advance and refresh methods to GaStatisticEngine

--- evolvekit/core/Ga/GaStatisticEngine.py
+++ evolvekit/core/Ga/GaStatisticEngine.py
@@ -1,7 +1,10 @@
 import time
+import numpy as np
+import copy
 
 from evolvekit.core.Ga import GaState
 from evolvekit.core.Ga.GaStatistics import GaStatistics
+from evolvekit.core.Ga.enums.GaExtremum import GaExtremum
 
 
 class GaStatisticEngine(GaStatistics):
@@ -18,7 +21,46 @@ class GaStatisticEngine(GaStatistics):
         self.last_time = self.start_time
 
     def advance(self, state: GaState):
-        pass
+        """
+        Advance the GA statistics by one generation.
+
+        :param state: GaState object
+        :returns: None
+        :raises: None
+        """
+        prev_best = self.best_indiv
+        self.refresh(state)
+
+        self.generation += 1
+        if prev_best and prev_best.value == self.best_indiv.value:
+            self.stagnation += 1
+        else:
+            self.stagnation = 0
+
+        self.last_time = time.process_time()
 
     def refresh(self, state: GaState):
-        pass
+        """
+        Refresh statistical metrics (mean, median, stddev) and update best/worst individuals.
+
+        :param state: GaState object
+        :returns: None
+        :raises: None
+        """
+        all_values = [ind.value for ind in state.current_population]
+
+        self.mean = np.mean(all_values)
+        self.median = np.median(all_values)
+        self.stdev = np.std(all_values)
+
+        key_func = lambda ind: ind.value
+        extremum = state.evaluator.extremum() if state.evaluator else GaExtremum.MAXIMUM
+        population = state.current_population
+
+        match extremum:
+            case GaExtremum.MAXIMUM:
+                self.best_indiv = copy.deepcopy(max(population, key=key_func))
+                self.worst_indiv = copy.deepcopy(min(population, key=key_func))
+            case GaExtremum.MINIMUM:
+                self.best_indiv = copy.deepcopy(min(population, key=key_func))
+                self.worst_indiv = copy.deepcopy(max(population, key=key_func))
