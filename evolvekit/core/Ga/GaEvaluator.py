57f31477a0ad93f41923ff9d242f88f4cb140446
Radosław Karpiński <100355451+AMDevRadeon@users.noreply.github.com>
2025-07-23 18:12:11 +0200

    Add documentation to GaEvaluator.py

--- evolvekit/core/Ga/GaEvaluator.py
+++ evolvekit/core/Ga/GaEvaluator.py
@@ -6,17 +6,56 @@ from evolvekit.core.Ga.enums.GaExtremum import GaExtremum
 
 
 class GaEvaluator(ABC):
+    """
+    Abstract class representing fitness function.
+    """
 
     @abstractmethod
     def evaluate(self, args: GaEvaluatorArgs) -> float:
+        """
+        Calculates the fitness value for a given set of arguments.
+
+        :param args: Object representing a particular solution for the
+        posited problem.
+        :type args: :class:`GaEvaluatorArgs`
+        :return: A value representing fitness for this particular
+        solution.
+        :rtype: float
+        """
         pass
 
     @abstractmethod
     def extremum(self) -> GaExtremum:
+        """
+        Returns optimalization criterion for the posited problem.
+
+        :return: A value signaling whether to search for minimum
+        or maximum in posited problem.
+        :rtype: :class:`GaExtremum`
+        """
         pass
 
     def real_domain(self) -> List[Tuple[float, float]]:
+        """
+        Returns the domain of the real valued chromosome.
+        If return value is set to an empty list, real valued
+        chromosome is ignored.
+
+        :return: A list of tuples representing lower and 
+        upper bounds, respectively, of every gene domain.
+        Defaults to an empty list.
+        :rtype: list
+        """
+
         return []
 
     def bin_length(self) -> int:
+        """
+        Returns bit length of the bit-string represented chromosome.
+        If return value is set to 0, bit-string represented
+        chromosome is ignored. Returned value must be nonnegative
+
+        :return: A length in bits. Defaults to 0.
+        :rtype: int
+        """
         return 0
