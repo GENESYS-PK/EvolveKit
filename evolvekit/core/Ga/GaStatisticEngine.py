5619bdb9533668b6d4df37d329a7b4b4f29f0034
Radosław Karpiński <100355451+AMDevRadeon@users.noreply.github.com>
2025-07-25 20:44:08 +0200

    Add documentation to GaStatisticEngine.py

--- evolvekit/core/Ga/GaStatisticEngine.py
+++ evolvekit/core/Ga/GaStatisticEngine.py
@@ -5,8 +5,21 @@ from evolvekit.core.Ga.GaStatistics import GaStatistics
 
 
 class GaStatisticEngine(GaStatistics):
+    """
+    Class responsible for manipulating and keeping track of
+    various statistics regarding the current run of genetic
+    algorithm.
+    """
 
     def start(self, state: GaState):
+        """
+        Initializes data class structure of :class:`GaStatisticEngine`
+        to default values.        
+
+        :param state: Object representing current state of evolution loop.
+        :type state: :class:`GaState`.
+        """
+
         self.generation = 1
         self.stagnation = 0
         self.mean = 0
@@ -18,7 +31,19 @@ class GaStatisticEngine(GaStatistics):
         self.last_time = self.start_time
 
     def advance(self, state: GaState):
+        """
+        Updates statistics AND increases generation number.
+
+        :param state: Object representing current state of evolution loop.
+        :type state: :class:`GaState`.
+        """
         pass
 
     def refresh(self, state: GaState):
+        """
+        Updates statistics WITHOUT increasing generation number.
+
+        :param state: Object representing current state of evolution loop.
+        :type state: :class:`GaState`.
+        """
         pass
