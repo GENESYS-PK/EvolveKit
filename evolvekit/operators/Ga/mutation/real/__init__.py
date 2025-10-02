a2eb6e46c5d06b7eb24ce686f9ee067f1a000fa5
Adrian Kulawik <a.kulawik8@gmail.com>
2025-10-02 11:46:11 +0200

    Add missing imports into __init__.py files

--- evolvekit/operators/Ga/mutation/real/__init__.py
+++ evolvekit/operators/Ga/mutation/real/__init__.py
@@ -1,6 +1,25 @@
 # Aliased Mutation Operators
+from evolvekit.operators.Ga.mutation.real.BoundaryMutation import BoundaryMutation
+from evolvekit.operators.Ga.mutation.real.BreederGAMutation import BreederGAMutation
+from evolvekit.operators.Ga.mutation.real.DynamicMutationA import DynamicMutationA
+from evolvekit.operators.Ga.mutation.real.DynamicMutationB import DynamicMutationB
+from evolvekit.operators.Ga.mutation.real.DynamicMutationC import DynamicMutationC
+from evolvekit.operators.Ga.mutation.real.DynamicMutationD import DynamicMutationD
+from evolvekit.operators.Ga.mutation.real.DynamicMutationE import DynamicMutationE
+from evolvekit.operators.Ga.mutation.real.UniformMutation import UniformMutation
+
 from evolvekit.operators.Ga.mutation.real.VirusInfectionMutation import (
     VirusInfectionMutation as RealVirusInfectionMutation,
 )
 
-__all__ = ["RealVirusInfectionMutation"]
+__all__ = [
+    "BoundaryMutation",
+    "BreederGAMutation",
+    "DynamicMutationA",
+    "DynamicMutationB",
+    "DynamicMutationC",
+    "DynamicMutationD",
+    "DynamicMutationE",
+    "UniformMutation",
+    "RealVirusInfectionMutation",
+]
