0f89687a73e9a8563270374f54a3fecc0202f656
Rafał Ciołek <rafal.ciolek12@gmail.com>
2025-10-11 23:32:38 +0200

    init test branch

--- evolvekit/__init__.py
+++ evolvekit/__init__.py
@@ -1,7 +1,51 @@
-# Components
-from evolvekit.core import *
+from .core.Ga.GaEvaluator import GaEvaluator
+from .core.Ga.GaEvaluatorArgs import GaEvaluatorArgs
+from .core.Ga.GaInspector import GaInspector
+from .core.Ga.GaResults import GaResults
+from .core.Ga.GaIndividual import GaIndividual
+from .core.Ga.GaIsland import GaIsland
+from .core.Ga.GaState import GaState
+from .core.Ga.GaStatisticEngine import GaStatisticEngine
+from .core.Ga.GaStatistics import GaStatistics
+from .core.Ga.enums.GaAction import GaAction
+from .core.Ga.enums.GaExtremum import GaExtremum
+from .core.Ga.enums.GaOpCategory import GaOpCategory
+from .core.Ga.enums.GaClampStrategy import GaClampStrategy
+from .core.Ga.operators.GaOperator import GaOperator
+from .core.Ga.operators.GaOperatorArgs import GaOperatorArgs
+from .core.Ga.helpers.ClampStrategy import get_clamp_strategy
+from .core.Ga.helpers.GaGenerateRandomPopulation import generate_random_population
+from .operators.Ga.crossover.binary.OnePointCrossover import (
+    OnePointCrossover as OnePointCrossoverBin,
+)
+from .operators.Ga.crossover.real.OnePointCrossover import OnePointCrossover
+from .operators.Ga.mutation.binary.VirusInfectionMutation import (
+    VirusInfectionMutation as VirusInfectionMutationBin,
+)
+from .operators.Ga.mutation.real.VirusInfectionMutation import VirusInfectionMutation
+from .operators.Ga.selection.RankSelection import RankSelection
 
-# Combine __all__
-from evolvekit.core import __all__ as core_all
-
-__all__ = core_all
+__all__ = [
+    "GaEvaluator",
+    "GaEvaluatorArgs",
+    "GaInspector",
+    "GaResults",
+    "GaIndividual",
+    "GaIsland",
+    "GaState",
+    "GaStatisticEngine",
+    "GaStatistics",
+    "GaAction",
+    "GaExtremum",
+    "GaOpCategory",
+    "GaClampStrategy",
+    "GaOperator",
+    "GaOperatorArgs",
+    "get_clamp_strategy",
+    "generate_random_population",
+    "OnePointCrossoverBin",
+    "OnePointCrossover",
+    "VirusInfectionMutation",
+    "VirusInfectionMutationBin",
+    "RankSelection",
+]
