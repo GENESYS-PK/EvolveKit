ed42e11775ef46a47eecf09b232991b70153984e
Marcin Fortuna <marcin.fortuna@onet.eu>
2026-02-02 19:52:09 +0100

    Move both BinaryEvaluator and MixedEvaluator to mocks

--- tests/unit/core/ga/evaluator/test_chromosome_types.py
+++ tests/unit/core/ga/evaluator/test_chromosome_types.py
@@ -6,55 +6,17 @@ chromosomes to ensure proper handling of different representations.
 """
 import pytest
 import numpy as np
-from typing import List, Tuple
-
-from evolvekit.core.Ga.GaEvaluator import GaEvaluator
 from evolvekit.core.Ga.GaEvaluatorArgs import GaEvaluatorArgs
 from evolvekit.core.Ga.GaIndividual import GaIndividual
-from evolvekit.core.Ga.enums.GaExtremum import GaExtremum
 from evolvekit.benchmarks.SphereEvaluator import SphereEvaluator
 from tests.utils.factories.individual_factories import (
     random_individual_factory,
     create_individual
 )
-
-
-class BinaryEvaluator(GaEvaluator):
-    """Evaluator that uses binary chromosome - counts 1s in bit string."""
-    
-    def __init__(self, bin_len: int = 10):
-        self._bin_len = bin_len
-    
-    def evaluate(self, args: GaEvaluatorArgs) -> float:
-        return float(np.sum(args.bin_chrom[:self._bin_len]))
-    
-    def extremum(self) -> GaExtremum:
-        return GaExtremum.MAXIMUM
-    
-    def bin_length(self) -> int:
-        return self._bin_len
-
-
-class MixedChromosomeEvaluator(GaEvaluator):
-    """Evaluator that uses both real and binary chromosomes."""
-    
-    def __init__(self, real_dim: int = 3, bin_len: int = 5):
-        self._real_dim = real_dim
-        self._bin_len = bin_len
-    
-    def evaluate(self, args: GaEvaluatorArgs) -> float:
-        real_part = np.sum(np.square(args.real_chrom[:self._real_dim]))
-        bin_part = np.sum(args.bin_chrom[:self._bin_len])
-        return float(real_part + bin_part)
-    
-    def extremum(self) -> GaExtremum:
-        return GaExtremum.MINIMUM
-    
-    def real_domain(self) -> List[Tuple[float, float]]:
-        return [(-5.0, 5.0)] * self._real_dim
-    
-    def bin_length(self) -> int:
-        return self._bin_len
+from tests.utils.mocks.mock_objects import (
+    MockBinaryEvaluator,
+    MockMixedEvaluator
+)
 
 
 class TestDifferentChromosomeTypes:
@@ -74,7 +36,7 @@ class TestDifferentChromosomeTypes:
         :raises: None
         """
         bin_len = len(bin_values)
-        evaluator = BinaryEvaluator(bin_len=bin_len)
+        evaluator = MockBinaryEvaluator(bin_len=bin_len)
         individual = GaIndividual(bin_chrom=np.array(bin_values, dtype=np.uint8))
         args = GaEvaluatorArgs(individual)
         
@@ -91,7 +53,7 @@ class TestDifferentChromosomeTypes:
         """
         real_dim = 2
         bin_len = 3
-        evaluator = MixedChromosomeEvaluator(real_dim=real_dim, bin_len=bin_len)
+        evaluator = MockMixedEvaluator(real_dim=real_dim, bin_len=bin_len)
         
         individual = GaIndividual(
             real_chrom=np.array([1.0, 2.0], dtype=np.float64),
@@ -110,7 +72,7 @@ class TestDifferentChromosomeTypes:
         """
         real_dim = 4
         bin_len = 6
-        evaluator = MixedChromosomeEvaluator(real_dim=real_dim, bin_len=bin_len)
+        evaluator = MockMixedEvaluator(real_dim=real_dim, bin_len=bin_len)
         
         assert len(evaluator.real_domain()) == real_dim
         assert evaluator.bin_length() == bin_len
